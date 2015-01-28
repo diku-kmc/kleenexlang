3489f24c63e887750c4bb59e10b5930f54b2a079
Ulrik Rasmussen <ulrik@utr.dk>
2015-01-28 15:53:57 +0100

    fst construction from mu expressions with sequential composition

---
+++ src/FSTConstruction.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE RankNTypes #-}
+module FSTConstruction
+(Mu(..)
+,fromMu)
+where
+
+import           Control.Monad.State
+import           Data.Monoid
+import qualified Data.Set as S
+
+import           SymbolicFST
+
+data ConstructState st pred func delta =
+  ConstructState { edges     :: [Edge st pred func delta]
+                 , nextState :: st
+                 , states    :: S.Set st
+                 }
+  deriving (Eq, Ord, Show)
+
+type Construct st pred func delta = State (ConstructState st pred func delta)
+
+data Mu pred func delta a = Var a
+                          | Loop (forall b. b -> Mu pred func delta b)
+                          | Alt (Mu pred func delta a) (Mu pred func delta a)
+                          | RW pred func (Mu pred func delta a)
+                          | W delta (Mu pred func delta a)
+                          | Seq (Mu pred func delta a) (Mu pred func delta a)
+                          | Accept
+
+fresh :: (Enum st, Ord st) => Construct st pred func delta st
+fresh = do
+  q <- gets nextState
+  modify $ \s -> s { nextState = succ q
+                   , states    = S.insert q (states s)
+                   }
+  return q
+
+addEdge :: st -> Either (pred, func) delta -> st -> Construct st pred func delta ()
+addEdge q lbl q' = modify $ \s -> s { edges = (q, lbl, q'):edges s }
+
+construct :: (Enum st, Ord st, Monoid delta)
+             => st -> Mu pred func delta st -> Construct st pred func delta st
+construct _ (Var q) = return q
+construct qf (Loop e) = mfix (construct qf . e)
+construct qf (RW p f e) = do
+  q' <- construct qf e
+  q <- fresh
+  addEdge q (Left (p, f)) q'
+  return q
+construct qf (W d e) = do
+  q' <- construct qf e
+  q <- fresh
+  addEdge q (Right d) q'
+  return q
+construct qf (Alt e1 e2) = do
+  q1 <- construct qf e1
+  q2 <- construct qf e2
+  q <- fresh
+  addEdge q (Right mempty) q1
+  addEdge q (Right mempty) q2
+  return q
+construct qf Accept = return qf
+construct qf (Seq e1 e2) = do
+  q2 <- construct qf e2
+  construct q2 e1
+
+fromMu :: (Enum st, Ord st, Monoid delta) =>
+          (forall a. Mu pred func delta a)
+       -> FST st pred func delta
+fromMu e =
+  let (qin, cs) = runState (construct (toEnum 0) e)
+                           (ConstructState { edges     = []
+                                           , nextState = toEnum 1
+                                           , states    = S.singleton (toEnum 0)
+                                           })
+  in FST { fstS = states cs
+         , fstE = edgesFromList (edges cs)
+         , fstI = qin
+         , fstF = S.singleton (toEnum 0)
+         }
