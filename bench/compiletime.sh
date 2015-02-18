a26726bc825c9c24f4fdf7bedd97466566e5a2b9
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-02-18 15:47:51 +0100

    Benchmark infrastructure for hased compiler

--- bench/compiletime.sh
+++ bench/compiletime.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # Measure compile times.
 
 # Assumes a link to the binary in ../repgbin
@@ -6,27 +6,60 @@ REPGC=../repgbin
 HASDIR=hased_src/
 OPT_LEVELS=(1 2 3)
 CCS=(gcc gcc-mp-4.9)
-REPS=5
+REPS=1
+BIN_POSTFIX=".benchbin"
+BIN_DIR="bin/"
+OUT_DIR="compiletimes/"
+COMPILETIME_POSTFIX=".compiletime"
 NAME=""
 SKIP="issuu"
-# SKIP="NONE"
+# SKIP="NONE" 
 
 # Args: file name, opt level, C compiler
 function setname {
-    NAME="${1}_${2}_${3}"
+    NAME="${1}__${2}__${3}"
 }
+
+function areyousure {
+    while true; do
+        read -p "This will clear old data in $OUT_DIR.  Proceed? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+cleardata=false
+while getopts ":c" opt; do
+  case $opt in
+  c)
+      cleardata=true
+      areyousure
+      ;;
+  \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit
+      ;;
+  esac
+done
 
 for opt_level in ${OPT_LEVELS[@]}; do
     for cc in ${CCS[@]}; do
         for n in $(ls $HASDIR); do
             if [[ ${n} !=  ${SKIP}* ]]; then
                 setname $n $opt_level $cc
-                cat /dev/null > $NAME.out # Make sure it's there and empty
+                timingdata=$OUT_DIR$NAME$COMPILETIME_POSTFIX
+                if [ "$cleardata" = true ]; then
+                    cat /dev/null > $timingdata
+                fi
                 for i in `seq 1 $REPS`; do
-                    CMD="$REPGC compile $HASDIR$n --out $NAME --opt $opt_level --cc $cc"
+                    binary=$BIN_DIR$NAME #$BIN_POSTFIX
+                    CMD="$REPGC compile $HASDIR$n --out $binary --opt $opt_level --cc $cc >> $timingdata"
                     echo $i
                     echo $CMD
-                    $CMD >> $NAME.out
+                    eval "$CMD"
                     echo ""
                 done
             fi
---
+++ bench/runningtime.sh
@@ -0,0 +1,70 @@
+#! /opt/local/bin/bash
+# Measure run time
+# Run after compiletime.sh, so the binaries are built.
+
+
+declare -A inputdata=(
+    ["simple_id.has"]="issuu/sample.json" #";issuu/medium.json"
+    ["id_ints.has"]="ints.txt"
+    ["patho1.has"]="abs_8.txt;abs9.txt"
+    ["flip_ab.has"]="abs_8.txt;abs9.txt"
+    ["issuu_fallback.has"]="issuu/big.json"
+    ["issuu_nofallback.has"]="issuu/big.json"
+)
+
+
+RUNTIME_POSTFIX=".runningtime"
+DATA_DIR="../test/data/"
+BIN_DIR="bin/"
+OUT_DIR="runningtimes/"
+REPS=2
+PRIMNAME=""
+
+# Set PRIMNAME to first component its argument
+function setprimname {
+    PRIMNAME=$(echo $1 | sed 's/^\(.*\)__\(.*\)__\(.*\)/\1/')
+}
+
+function areyousure {
+    while true; do
+        read -p "This will clear old data in $OUT_DIR.  Proceed? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+cleardata=false
+while getopts ":c" opt; do
+  case $opt in
+  c)
+      cleardata=true
+      areyousure
+      ;;
+  \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit
+      ;;
+  esac
+done
+
+
+for bin in $(ls $BIN_DIR); do
+    setprimname $bin
+    IFS=';' read -a inputs <<< ${inputdata[$PRIMNAME]}
+    for input in ${inputs[@]}; do
+        outfile=$OUT_DIR$bin${RUNTIME_POSTFIX}
+        if [ "$cleardata" = true ]; then
+            cat /dev/null > $outfile
+        fi
+        for i in `seq 1 $REPS`; do
+            CMD="$BIN_DIR$bin -t < ${DATA_DIR}${input} > /dev/null 2>> $outfile"
+            echo $i
+            echo $CMD
+            eval "$CMD"
+            echo ""
+        done
+    done
+done
