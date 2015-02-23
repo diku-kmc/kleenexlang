f18514d6f7d36bfbb7842a29521b81081ea12732
Ulrik Rasmussen <ulrik@utr.dk>
2015-02-23 17:24:22 +0100

    shorter code generation

--- src/KMC/Program/IL.hs
+++ src/KMC/Program/IL.hs
@@ -22,6 +22,7 @@ newtype BufferId = BufferId { getBufferId :: Int } deriving (Eq, Ord, Show)
 data Expr =
     SymE Int          -- ^ next[i] (i less than value of AvailableSymbolsE)
   | AvailableSymbolsE -- ^ Number of available symbols
+  | CompareE Int [Int] -- compare(&next[i], str, length(str))
   | ConstE Int
   | FalseE
   | TrueE
