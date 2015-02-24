67d0a678e73422d8ef4a799e5c43c9a0bc38ce66
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-02-24 16:45:35 +0100

    More Perl progs!

---
+++ bench/perl/src/patho2.pl
@@ -0,0 +1,35 @@
+#! env perl
+use strict;
+use warnings;
+use Time::HiRes qw/ time /;
+
+# Perl variant of patho2.{cpp,has,py}
+
+my $regex = "(?:([a-z]*a)|([a-z]*b))?(?:\n)?";
+
+my $pre_compile = time;
+
+# NOTE THE ADDED ANCHORS
+my $pattern = qr/^$regex$/;
+my $lno = 0;
+
+# Start timing
+my $start = time;
+
+while (<STDIN>) {
+    $lno = $lno + 1;
+    if ($_ =~ $pattern) {
+        print STDOUT sprintf("%s\n", defined $2? $2 : "");
+    } else {
+        print STDERR "match error on line ${lno}\n";
+        exit 1;
+    }
+}
+
+# End timing
+my $end = time;
+my $elaps = int(($end - $start) * 1000);
+my $elaps_compile = int(($start - $pre_compile) * 1000);
+
+print STDERR "\ncompilation (ms): ${elaps_compile}\n";
+print STDERR "matching (ms):    ${elaps}\n"
---
+++ bench/perl/src/simple_id.pl
@@ -0,0 +1,35 @@
+#! env perl
+use strict;
+use warnings;
+use Time::HiRes qw/ time /;
+
+# Perl variant of simple_id.{cpp,has,py}
+
+my $regex = "(.*)";
+
+my $pre_compile = time;
+
+# Note the /s modifier.  This makes dot match newline.
+my $pattern = qr/$regex/s;
+my $lno = 0;
+
+# Start timing
+my $start = time;
+
+while (<STDIN>) {
+    $lno = $lno + 1;
+    if ($_ =~ $pattern) {
+        print STDOUT sprintf("%s", $1);
+    } else {
+        print STDERR "match error on line ${lno}\n";
+        exit 1;
+    }
+}
+
+# End timing
+my $end = time;
+my $elaps = int(($end - $start) * 1000);
+my $elaps_compile = int(($start - $pre_compile) * 1000);
+
+print STDERR "\ncompilation (ms): ${elaps_compile}\n";
+print STDERR "matching (ms):    ${elaps}\n"
