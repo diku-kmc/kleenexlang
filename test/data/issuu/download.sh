3921aeebc48f76652034e33600cc88aca5c4910c
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-02-27 18:31:53 +0100

    Download script for Issuu data

---
+++ test/data/issuu/download.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Download script that fetches and unpacks the data set from Issuu.
+# To actually start the download, set dry_run to true.  The .xz files
+# take up about 5-7 GB each, and the uncompressed .json files between
+# 250 and 350 GB.  
+
+dry_run=true
+data_chunks=( 2014-03-01.json.xz
+              # 2014-03-02.json.xz
+              # 2014-03-03.json.xz
+              # 2014-03-04.json.xz
+              # 2014-03-05.json.xz
+              # 2014-03-06.json.xz
+              # 2014-03-07.json.xz
+              # 2014-03-08.json.xz
+              # 2014-03-09.json.xz
+              # 2014-03-10.json.xz
+              # 2014-03-11.json.xz
+              # 2014-03-12.json.xz
+              # 2014-03-13.json.xz
+              # 2014-03-14.json.xz
+              # 2014-03-15.json.xz
+              # 2014-03-16.json.xz
+              # 2014-03-17.json.xz
+              # 2014-03-18.json.xz
+              # 2014-03-19.json.xz
+              # 2014-03-20.json.xz
+              # 2014-03-21.json.xz
+              # 2014-03-22.json.xz
+              # 2014-03-23.json.xz
+              # 2014-03-24.json.xz
+              # 2014-03-25.json.xz
+              # 2014-03-26.json.xz
+              # 2014-03-27.json.xz
+              # 2014-03-28.json.xz
+              # 2014-03-29.json.xz
+              # 2014-03-30.json.xz
+              # 2014-03-31.json.xz
+            )
+
+for data_chunk in ${data_chunks[@]} ; do
+    if [ ! -f $data_chunk ] ; then
+        wget="wget http://labs.issuu.com/anodataset/${data_chunk}"
+        xz="xz --decompress ${data_chunk}"
+        cmd="$wget && $xz"
+        echo $cmd
+        if [ "$dry_run" = false ] ; then
+            eval "$cmd"
+        fi
+    fi
+done
+
