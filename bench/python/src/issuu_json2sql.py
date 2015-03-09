515e35ad359a3e4dbdda22aac4f3db84d0834dcd
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-03-09 18:33:29 +0100

    Benchmark script for all programs; specify input externally.

---
+++ bench/python/src/issuu_json2sql.py
@@ -0,0 +1,54 @@
+#! env python
+
+# Python version the issuu_json2sql hased program.
+
+# This implementation uses the Python library "json" to convert the .json file
+# to a SQL insertion statement.  This might seem like comparing apples and oranges,
+# as this program does not exercise the Python regex engine at all (at least not
+# directly), but if this program is to be a realistic use of Python it has to
+# be "Pythonic" and use the standard library function exposed by the json module.
+# Thus, comparing the hased version with the Python version is not a comparison
+# of regex engines, but of two different ways of solving the same problem.
+# This all comes with the caveat that this Python implementation is much more
+# expressive than the hased program because it has the full computational
+# expressibility of Python available.  As a consequence, the hased version
+# only works under some assumptions on the input json data, notably that the
+# fields come in a certain order and that they all are present in every object.
+
+import sys
+import re
+import datetime
+import json
+
+lno = 0
+
+# Start timing
+start = datetime.datetime.now()
+
+sys.stdout.write("INSERT INTO issuu_log (ts, visitor_uuid, visitor_useragent, visitor_country) VALUES\n")
+first = True
+for line in sys.stdin:
+    lno += 1
+    o = json.loads(line)
+    if not first:
+        sys.stdout.write(",\n")
+    first = False
+    if o:
+        sys.stdout.write("(%s,'%s','%s','%s')" %
+                         (o["ts"],
+                          o["visitor_uuid"],
+                          o["visitor_useragent"],
+                          o["visitor_country"]))
+    else:
+        sys.stderr.write("json object could not be loaded.")
+        exit(1)
+
+sys.stdout.write(";\n")
+# End timing
+end = datetime.datetime.now()
+
+# Elapsed time
+elaps = end - start
+elaps_ms = elaps.seconds * 1000 + elaps.microseconds / 1000
+
+sys.stderr.write("\nmatching (ms):    %s\n" % str(elaps_ms))
--- bench/runningtime.sh
+++ bench/runningtime.sh
@@ -2,33 +2,38 @@
 # Measure run time
 # Run after compiletime.sh, so the binaries are built.
 
-
-declare -A inputdata=(
-    ["simple_id.has"]="issuu/sample.json" #";issuu/medium.json"
-    ["id_ints.has"]="ints.txt"
-    ["patho1.has"]="ab_strings/ab1.txt"
-    ["flip_ab.has"]="ab_strings/ab1.txt"
-    ["issuu_fallback.has"]="issuu/big.json"
-    ["issuu_nofallback.has"]="issuu/big.json"
-    ["csv2json.has"]="csv/csv_format1.csv"
-    ["csv2json_nows.has"]="csv/csv_format1.csv"
-    ["json2csv.has"]="csv/csv_format1.csv.ws.json;csv/csv_format1.csv.nows.json"
-    ["as.has"]="a_strings/as.txt"
-)
+test_case_table="${BASH_SOURCE%/*}/benchmarks.txt"
+input_table="${BASH_SOURCE%/*}/inputs.txt"
+all_test_cases=$(cat ${test_case_table} | awk '$1 !~ /#/ { print $1 }')
 
 
-RUNTIME_POSTFIX=".runningtime"
-DATA_DIR="../test/data/"
-BIN_DIR="bin/"
-OUT_DIR="runningtimes/"
-REPS=1 # number of repetitions of each run
-PRIMNAME=""
-
-
-# Set PRIMNAME to first component its argument
-function setprimname {
-    PRIMNAME=$(echo $1 | sed 's/^\(.*\)__\(.*\)__\(.*\)/\1/')
+# Get invocation command
+function invocation {
+    prog=$2
+    if   [ $1 == "hased"     ]; then cmd="hased/bin/$prog -t"
+    elif [ $1 == "re2"       ]; then cmd="re2/bin/$prog"
+    elif [ $1 == "re2j"      ]; then cmd="java -jar re2j/build/jar/$prog.jar"
+    elif [ $1 == "gawk"      ]; then cmd="gawk/src/$prog.awk"
+    elif [ $1 == "python"    ]; then cmd="python python/src/$prog.py"
+    elif [ $1 == "perl"      ]; then cmd="perl perl/src/$prog.pl"
+    elif [ $1 == "tcl"       ]; then cmd="tcl/src/$prog.tcl"
+    elif [ $1 == "sed"       ]; then cmd="sed/src/$prog.sh"
+    elif [ $1 == "grep"      ]; then cmd="grep/src/$prog.sh"
+    elif [ $1 == "cpp11"     ]; then cmd="cpp11/bin/$prog"
+    elif [ $1 == "oniguruma" ]; then cmd="oniguruma/bin/$prog"
+    else
+        echo "Could not find invocation for $1"
+        exit 1
+    fi
+    printf "$cmd"
 }
+
+
+time_postfix=".runningtime"
+data_dir="../test/data/"
+time_dir="times/"
+warmup_reps=0
+reps=1 # number of repetitions of each run
 
 function areyousure {
     while true; do
@@ -44,63 +49,78 @@ function areyousure {
 
 prefix=""
 cleardata=false
-dryrun=false
-only_do=""
-while getopts ":dp:cn:o:" opt; do
-  case $opt in
-  c)
-      cleardata=true
-      areyousure "This will clear old data in $OUT_DIR.  Proceed? "
-      ;;
-  p)
-      prefix=$OPTARG
-      ;;
-  o)
-      echo "# Only doing $OPTARG"
-      only_do=$OPTARG
-      ;;
-  n)      
-      areyousure "This will delete anything in $OUT_DIR prefixed by $OPTARG.  Proceed? "
-      rm $OUT_DIR$OPTARG*
-      exit
-      ;;
-  d)
-      dryrun=true
-      echo "# Making a dry-run..."
-      ;;
-  \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit
-      ;;
-  esac
+dryrun=true
+only_case=""
+only_prog=""
+
+while getopts ":fc:p:w:r:" opt; do
+    case $opt in
+        c)
+            echo "# Only doing case $OPTARG"
+            only_case=$OPTARG
+            ;;
+        p)
+            echo "# Only doing program $OPTARG"
+            only_prog=$OPTARG
+            ;;
+        f)
+            dryrun=false
+            echo "# NOT making a dry-run..."
+            ;;
+        w)
+            echo "# Doing $OPTARG warm-up runs."
+            warmup_reps=$OPTARG
+            ;;
+        r)
+            echo "# Doing $OPTARG benchmark runs."
+            reps=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit
+            ;;
+    esac
 done
 
-function run_re2 {
-    
-}
-
-for bin in $(ls $BIN_DIR); do
-    setprimname $bin
-    if [ "$only_do" != "" ]; then
-        if [ $only_do != $PRIMNAME ]; then
-            continue;
-        fi
-    fi
-    IFS=';' read -a inputs <<< ${inputdata[$PRIMNAME]}
+function run {
+    testname=$1
+    flavor=$2
+    # Get names of input files.
+    IFS=';' read -a inputs <<< $(cat ${input_table} | awk "\$1 ~ /${testname}/ { print \$2 }")
+    out_dir="$flavor/$time_dir"
+    mkdir -p $out_dir
+    printf "# Called with %s %s\n" $testname $flavor
     for input in ${inputs[@]}; do
         pf=$(echo $input | sed 's/\//_/g')
-        outfile=$OUT_DIR$prefix$bin-$pf${RUNTIME_POSTFIX}
-        if [ "$cleardata" = true ]; then
-            cat /dev/null > $outfile
+        outfile="${out_dir}${prefix}${testname}-${pf}${time_postfix}"
+        _cmd="$(invocation $flavor $testname) < ${data_dir}${input} > /dev/null"
+        warmup_cmd="$_cmd 2> /dev/null"
+        cmd="$_cmd 2>> ${outfile}"
+        if [ $warmup_reps -gt 0 ]; then
+            for i in $(seq 1 $warmup_reps); do
+                echo "# WARMUP #$i: "
+                echo $warmup_cmd
+                if [ "$dryrun" = false ]; then eval $warmup_cmd ; fi
+            done
         fi
-        for i in `seq 1 $REPS`; do
-            CMD="$BIN_DIR$bin -t < ${DATA_DIR}${input} > /dev/null 2>> $outfile"
-            echo "#$i"
-            echo $CMD
-            if [ "$dryrun" = false ]; then
-                eval "$CMD"
-            fi
-            echo ""
+        for i in $(seq 1 $reps); do
+            echo "# Run #$i: "
+            echo $cmd
+            if [ "$dryrun" = false ]; then eval $cmd ; fi
         done
     done
+}
+
+
+for test_case in $all_test_cases; do
+    IFS=',' read -a progs <<< $(cat ${test_case_table} | awk "\$1 ~ /${test_case}/ { print \$2 }")
+    if [ "$only_case" != "" ] && [ "$only_case" == "$test_case" ] ||
+           [ "$only_case" == "" ]; then
+        for prog in $progs; do
+            if [ "$only_prog" != "" ] && [ "$only_prog" == $prog ] ||
+                   [ "$only_prog" == "" ] ; then
+                run $test_case $prog
+            fi
+        done
+    fi
 done
