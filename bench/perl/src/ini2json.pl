bcd6b3b0bc243c790b388c5162a57b8beed7544a
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2015-03-16 11:00:41 +0100

    Add ini2json to Makefile; speical-cased

--- bench/perl/src/ini2json.pl
+++ bench/perl/src/ini2json.pl
@@ -1,6 +1,22 @@
 #!/usr/bin/env perl
 use 5.012;
 use warnings;
+use Time::HiRes qw/time/;
+
+# Perl version of the INI 2 JSON conversion.
+# Quite differently structured from the Kleenex version.
+
+my $pre_compile = time;
+
+my $commentRx  = qr/^\s*;|^\s*$/;
+my $headingRx  = qr/^\s*\[([^\n\]]*)\]\s*$/;
+my $keyValueRx = qr/^\s*([^;\s=\[]*)\s*=\s*([^\n]*)\s*$/;
+my $quotedRx   = qr/^".*"$/;
+my $bslashRx   = qr/\\/;
+my $quoteRx    = qr/"/;
+
+# Start timing
+my $start = time;
 
 print "{\n";
 
@@ -11,9 +27,9 @@ my $ind = "    ";
 my $i = 0;
 while (my $line = <>) {
     $i ++;
-    next if ($line =~ qr/^\s*;|^\s*$/);
+    next if ($line =~ $commentRx);
 
-    if (my ($heading) = $line =~ qr/^\s*\[([^\n\]]*)\]\s*$/) {
+    if (my ($heading) = $line =~ $headingRx) {
         print "\n$ind},\n" unless $firstSection;
         $firstSection = 0;
         $firstKeyValuePair = 1;
@@ -23,10 +39,10 @@ while (my $line = <>) {
 
     die("Needs to start with a section before keys (line $i)") if $firstSection;
 
-    if (my ($key, $value) = $line =~ qr/^\s*([^;\s=\[]*)\s*=\s*([^\n]*)\s*$/) {
-        unless ($value =~ qr/^".*"$/) {
-            $value =~ s/\\/\\\\/g;
-            $value =~ s/"/\\"/g;
+    if (my ($key, $value) = $line =~ $keyValueRx) {
+        unless ($value =~ $quotedRx) {
+            $value =~ s/$bslashRx/\\\\/g;
+            $value =~ s/$quoteRx/\\"/g;
             $value = qq{"$value"};
         }
 
@@ -38,3 +54,11 @@ while (my $line = <>) {
 
 print "\n$ind}" unless $firstSection;
 print "\n}\n";
+
+# End timing
+my $end = time;
+my $elaps = int(($end - $start) * 1000);
+my $elaps_compile = int(($start - $pre_compile) * 1000);
+
+print STDERR "\ncompilation (ms): ${elaps_compile}\n";
+print STDERR "matching (ms):    ${elaps}\n"
