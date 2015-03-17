1829b6184377d512767a766a04dce76330bfb056
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-03-17 13:46:02 +0100

    Installation scripts

--- bench/re2j/getant.sh
+++ bench/re2j/getant.sh
@@ -3,6 +3,11 @@
 echo "Checking if 'ant' is installed on system..."
 if hash ant 2>/dev/null; then
     echo "'ant' is already installed."
+    echo "Making ./ant point to system ant"
+    rm -f ant
+    echo "#!/bin/sh" >> ant
+    echo "ant \$@" >> ant
+    chmod +x ant
     exit 0
 fi
 echo "'ant' is not installed; installing it locally."
@@ -17,5 +22,6 @@ fi
 if [ ! -d $antdir ]; then
     echo "Unpacking ant..."
     tar xfz $antball
+    echo "Making ./ant point to newly installed ant"
     ln -s $antdir/bin/ant ant
 fi
