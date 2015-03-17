b8b3de4227359949b0bdc6a8cbe94656ed61d2e1
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-03-17 14:19:53 +0100

    Fix ragel timing output

--- bench/ragel/common.h
+++ bench/ragel/common.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 #define BUFFER_SIZE (200*1024*1024)
 char buffer[BUFFER_SIZE] = {0};
@@ -13,7 +14,7 @@ char buffer[BUFFER_SIZE] = {0};
 
 #define POST                                                \
   uint64_t stop = getTimeMs();                              \
-  fprintf(stderr, "\nmatching (ms): %llu\n", stop - start);
+  fprintf(stderr, "\nmatching (ms): %" PRIu64 "\n", stop - start);
 
 
 uint64_t getTimeMs() {
