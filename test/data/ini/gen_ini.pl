fb1b76af8737991d9d43778963640f37b3632378
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-03-18 15:56:11 +0100

    Generate a fixed amount of inifile.

--- test/data/ini/gen_ini.pl
+++ test/data/ini/gen_ini.pl
@@ -1,6 +1,19 @@
 #!/usr/bin/env perl
 use 5.012;
 use warnings;
+use Getopt::Long;
+
+=head1 USAGE
+
+Generate approx. 10MB ini file.
+
+    gen_ini.pl -s 10000000
+
+Generate an endless stream of ini file.
+
+    gen_ini.pl
+
+=cut
 
 my @comm_chars = ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9', '=', ' ' );
 my @sect_chars = ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9', '=', '_', "-" );
@@ -9,56 +22,74 @@ my @value_chars = ( 'a' .. 'z', 'a' .. 'z', '0' .. '9', '=', '_', "-", ' ' );
 my @value_chars_quot = ( 'a' .. 'z', 'a' .. 'z', '0' .. '9', '=', '_', "-", ' ');
 
 sub comment {
+    my $r = "";
     if (rand() < 0.9) {
-        print ";";
+        $r = $r . ";";
         for my $i (1 .. rand(200)) {
-            print $comm_chars[rand @comm_chars];
+            $r = $r . $comm_chars[rand @comm_chars];
         }
-        print "\n";
+        $r = $r . "\n";
     } else {
-        print " "x(rand(100)) . "\n";
+        $r = $r . " "x(rand(100)) . "\n";
     }
+    return $r;
 }
 
 sub comments {
+    my $r = "";
     if (rand() > 0.3) {
-        for my $i (1 .. rand(5)) { comment; }
+        for my $i (1 .. rand(5)) { $r = $r . comment; }
     }
+    return $r;
 }
 
 sub section {
-    print "[";
+    my $r = "";
+    $r = $r . "[";
     for my $i (1 .. rand(50)) {
-        print $sect_chars[rand @sect_chars];
+        $r = $r . $sect_chars[rand @sect_chars];
     }
-    print "]\n";
+    $r = $r . "]\n";
+    return $r;
 }
 
 sub keyvalue {
+    my $r = "";
     for my $i (1 .. rand(30)) {
-        print $key_chars[rand @key_chars];
+        $r = $r . $key_chars[rand @key_chars];
     }
-    print " "x(rand(3)) . "=" . " "x(rand(3));
+    $r = $r . " "x(rand(3)) . "=" . " "x(rand(3));
     if (rand() < 0.7) {
         for my $i (1 .. rand(100)) {
-            print $value_chars[rand @value_chars];
+            $r = $r . $value_chars[rand @value_chars];
         }
     } else {
-        print '"';
+        $r = $r . '"';
         for my $i (1 .. rand(100)) {
-            print $value_chars_quot[rand @value_chars_quot];
+            $r = $r . $value_chars_quot[rand @value_chars_quot];
         }
-        print '"';
+        $r = $r . '"';
     }
-    print "\n";
+    $r = $r . "\n";
+    return $r;
 }
 
+# Approx. number of bytes to generate.
+my $b = undef;
+GetOptions("size_bytes=i" => \$b);
+
 while (1) {
-    comments;
-    section;
+    my $r = comments;
+    $r = $r . section;
+    print $r;
+
+    $b -= length($r) if (defined $b);
 
     for my $i (1 .. rand(20)) {
-        comment if (rand() < 0.3);
-        keyvalue;
+        $r = comment if (rand() < 0.3);
+        $r = $r . keyvalue;
+        print $r;
+        $b -= length($r) if (defined $b);
     }
+    last if (defined $b && $b <= 0);
 }
