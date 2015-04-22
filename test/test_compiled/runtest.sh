55db3e73b0285895d8f7a00e163d81303f228224
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-04-22 12:42:39 +0200

    Remove whitespace after colon

--- test/test_compiled/runtest.sh
+++ test/test_compiled/runtest.sh
@@ -12,8 +12,8 @@ for tst in $(ls $test_dir); do
     fi
     compile="${repgc} compile --opt 0 --la=false --out ${tst}_bin $test_dir/${tst}"
     eval "$compile > /dev/null"
-    input=$(sed -n 's/\/\/ IN: \(.*\)/\1/p' < $test_dir/$tst)
-    exp_out=$(sed -n 's/\/\/ OUT: \(.*\)/\1/p' < $test_dir/$tst)
+    input=$(sed -n 's/\/\/ IN:\(.*\)/\1/p' < $test_dir/$tst)
+    exp_out=$(sed -n 's/\/\/ OUT:\(.*\)/\1/p' < $test_dir/$tst)
     out=$(echo "$input" | ./"$tst"_bin)
     if [[ $out == $exp_out ]]; then
         echo "$tst Pass"
