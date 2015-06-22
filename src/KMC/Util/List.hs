f9cedfac6a3c8607acc4dcc9efec46f510935dc7
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-06-22 12:58:05 +0200

    "alternate" utility function.

--- src/KMC/Util/List.hs
+++ src/KMC/Util/List.hs
@@ -4,3 +4,11 @@ module KMC.Util.List where
 foldr1ifEmpty :: (a -> a -> a) -> a -> [a] -> a
 foldr1ifEmpty _ e [] = e
 foldr1ifEmpty f _ l  = foldr1 f l
+
+-- | Mix two lists together by alternating between elements of the first
+-- and the second.  The resulting list will be twice the length of the
+-- shortest of the argument lists.
+alternate :: [a] -> [a] -> [a]
+alternate [] _      = []
+alternate (x:xs) ys = x : alternate ys xs
+
