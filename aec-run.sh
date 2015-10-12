321d6c6ac30c26987e6fa99514978066c13d5697
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2015-10-12 16:02:01 +0200

    Don't generate test data in aec script

--- aec-run.sh
+++ aec-run.sh
@@ -8,23 +8,11 @@ timestamp="$(date "+%Y_%m_%d__%k_%M_%S")"
 
 cd bench
 
-
-
-# 1.  Generate some test data. 
-while true; do
-    read -p "Do you wish to generate the test data? " yn
-    case $yn in
-        [Yy]* ) make generate-aec-data; break;;
-        [Nn]* ) echo "Not generating new test data..."; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# 2.  Run some of the benchmark programs
+# 1.  Run some of the benchmark programs
 echo ""
 echo "######################################"
 while true; do
-    read -p "Do you wish to run the benchmark runs? " yn
+    read -p "Do you wish to /run/ the benchmark runs, as opposed to seeing what would be run? " yn
     case $yn in
         [Yy]* ) go="-f"; break;;
         [Nn]* ) echo "Only making a dry-run showing what would've been done..."; break;;
@@ -40,7 +28,7 @@ done
 ./runningtime.sh $go -w 0 -r 1 -l $timestamp -p csv2json
 ./runningtime.sh $go -w 0 -r 1 -l $timestamp -p isp_datetime_to_json
 
-# 3.  Make some plots
+# 2.  Make some plots
 echo ""
 echo "######################################"
 while true; do
